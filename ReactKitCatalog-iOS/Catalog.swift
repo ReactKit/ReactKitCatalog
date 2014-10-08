f3188a773597d2802206a2eac91f93bbef858949
Yasuhiro Inami <inamiy@gmail.com>
2014-10-08 17:19:12 +0900

    Add BarButtonItem iOS sample.

--- ReactKitCatalog-iOS/Catalog.swift
+++ ReactKitCatalog-iOS/Catalog.swift
@@ -18,6 +18,7 @@ struct Catalog
     {
         return [
             Catalog(title: "NSTimer", description: nil, class_: TimerViewController.self),
+            Catalog(title: "UIBarButtonItem", description: nil, class_: BarButtonItemViewController.self),
             Catalog(title: "UIButton", description: nil, class_: ButtonViewController.self),
             Catalog(title: "UITextField", description: nil, class_: TextFieldViewController.self),
             Catalog(title: "UITextField (Multiple)", description: nil, class_: MultipleTextFieldViewController.self),
