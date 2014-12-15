b96392bab32998f644724aa76410b78fcb07813d
Yasuhiro Inami <inamiy@gmail.com>
2014-12-15 10:01:24 +0900

    [TextField] Add throttle/debounce examples.

--- ReactKitCatalog-iOS/Samples/TextFieldViewController.swift
+++ ReactKitCatalog-iOS/Samples/TextFieldViewController.swift
@@ -12,20 +12,32 @@ import ReactKit
 class TextFieldViewController: UIViewController
 {
     @IBOutlet var label: UILabel!
+    @IBOutlet var throttleLabel: UILabel!
+    @IBOutlet var debounceLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
     var signal: Signal<NSString?>?
+    var throttleSignal: Signal<NSString?>?
+    var debounceSignal: Signal<NSString?>?
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         self.signal = self.textField?.textChangedSignal()
+        self.throttleSignal = self.signal?.throttle(1).map { text -> NSString? in "\(text!) (throttled)" }
+        self.debounceSignal = self.signal?.debounce(1).map { text -> NSString? in "\(text!) (debounced)" }
         
         // REACT: textField ~> label
         (self.label, "text") <~ self.signal!
         
-        // REACT: textField ~> println
-        ^{ println($0!) } <~ self.signal!
+        // REACT: textField ~> throttleLabel
+        (self.throttleLabel, "text") <~ self.throttleSignal!
+        
+        // REACT: textField ~> debounceLabel
+        (self.debounceLabel, "text") <~ self.debounceSignal!
+        
+//        // REACT: textField ~> println
+//        ^{ println($0!) } <~ self.signal!
     }
 }
